9799c56325a3f89dc464e0c8556503b02c8bdabe
Atsuya Tanaka <tenmusu007@gmail.com>
2022-09-08 12:32:25 -0700

    add

--- w1/d1/server.js
+++ w1/d1/server.js
@@ -3,11 +3,23 @@ const http = require("http");
 const server = http.createServer((request, response) => {
   console.log("A wild client appears!", request.url);
 
-  if (request.url === "/test") {
+  if (request.url === "/users") {
+    response.writeHead(200, { "Content-type": "applicaiton/json" });
+    response.write(
+      JSON.stringify({
+        user: {
+          id: 1,
+          name: "atsuya",
+          age : 23
+      }})
+		);
+  } else if (request.url === "/products") {
     response.writeHead(200, { "Content-type": "text/html" });
-    response.write("<h1>Hello Client in test!!!!!!</h1>");
-  }
+    response.write(
+			"<h1>cat!!!!!!</h1><img src='https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg?q=60'alt=''/>;"
+		);
+	}
   response.end();
 });
 
-server.listen(3000, () => console.log("Server is running at port 3000"));
+server.listen(8000, () => console.log("Server is running at port 8000"));
