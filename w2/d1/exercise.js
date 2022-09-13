f6d186c28d529f269656fcd029af960c7523df3f
Arthur Di Lascio <arthur.dilascio@gmail.com>
2022-09-13 10:29:16 -0700

    w2d2 examples

--- w2/d1/exercise.js
+++ w2/d1/exercise.js
@@ -14,5 +14,61 @@
  * STRETCH (BONUS): The server should have a JSON file that stores the pets.
  *
  */
+const bodyParser = require("body-parser");
+const express = require("express");
+const morgan = require("morgan");
 
-const pets = [{ id: 1, name: "Zoey", age: 1, type: "dog" }];
+let pets = [{ id: 1, name: "Zoey", age: 1, type: "dog" }];
+
+const app = express();
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("pets", { pets });
+});
+
+app.get("/new", (req, res) => {
+  res.render("newPet");
+});
+
+app.post("/new", (req, res) => {
+  const body = req.body;
+  console.log("body", body);
+  const newPet = { ...req.body, id: pets.length + 1 };
+  pets.push(newPet);
+  res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+  console.log("params", req.params);
+  //   const id = Number(req.params.id)
+  const id = +req.params.id;
+  console.log("id", id);
+  const petSelected = pets.find((pet) => pet.id === id);
+  res.render("editPet", { pet: petSelected });
+});
+
+app.post("/edit/:id", (req, res) => {
+  const id = +req.params.id;
+  const updatedPet = { ...req.body };
+  console.log(pets);
+  pets = pets.map((pet) => {
+    if (pet.id === id) {
+      return { ...pet, ...updatedPet };
+    }
+    return pet;
+  });
+
+  res.redirect("/");
+});
+
+app.post("/delete/:id", (req, res) => {
+  const id = +req.params.id;
+  pets = pets.filter((pet) => pet.id !== id);
+  res.redirect("/");
+});
+
+app.listen(3001, () => console.log("server running 3001"));
